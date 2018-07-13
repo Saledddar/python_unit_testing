07b22d7bc12c232191433e30f3fc864d3f52d8ab
Daradji Salah Eddine <saledddar@gmail.com>
2018-07-13 20:52:06 +0100

    First release

--- setup.py
+++ setup.py
@@ -4,18 +4,25 @@ import sys, os
 README = open('README.md').read()
 
 setup(name='saltools',
-    version='0.1',
-    description='',
+    version='0.0.1',
+    description='A collection of useful tools',
     long_description=README,
-    classifiers=[],
-    keywords='',
-    author='',
-    author_email='',
+    long_description_content_type='text/markdown',
+    classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: Microsoft :: Windows',
+          'Programming Language :: Python',
+          'Topic :: Software Development :: Libraries',
+      ],
+    keywords='tools code useful',
+    author='saledddar',
+    author_email='saledddar@gmail.com',
     url='https://github.com/Saledddar/saltools',
     license='MIT',
-    packages=find_packages('src'),
-    package_dir = {'': 'src'},
     include_package_data=True,
     zip_safe=False,
-    install_requires=[]
+    packages = ['saltools'],
+    install_requires=['lxml','requests', 'pyunet']
 )
--- src/saltools/__main__.py
+++
@@ -1,8 +0,0 @@
-"""
-Entry point of the package
-"""
-import argparse
-
-#Set the arg parser
-parser = argparse.ArgumentParser(description='A collection of tools',prog='python -m saltools')
-parser.add_argument('name'  ,metavar='N'    ,default = ''            ,   help='The name of the package'                      )
